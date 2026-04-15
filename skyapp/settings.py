50fd20a9fee17905d964d74db8bca8fae8503f8c
Mithila240 <hmithila240@gmail.com>
2026-04-15 21:19:36 +0100

    Rename messages app to messaging

--- skyapp/settings.py
+++ skyapp/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'teams',
+    'organisation',
+    'messaging',   
+    'schedule',
+    'reports',
+    'visualisation',
 ]
 
 MIDDLEWARE = [
--- teams/models.py
+++ teams/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Team(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    email = models.EmailField(blank=True)
+    skills = models.TextField(blank=True)
+    dependencies = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
