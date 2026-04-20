dcb93620ba25499bed05777ba708340b63f3e686
G-774 <goufranm774@gmail.com>
2026-04-20 04:48:14 +0100

    update messaging templates and views

--- messaging/urls.py
+++ messaging/urls.py
@@ -3,8 +3,9 @@ from . import views
 
 urlpatterns = [
 
-    path("", views.messaging_home, name="messaging_home"),
-    path('send/', views.send_message, name='send_message'),
-    path('inbox/', views.inbox, name='inbox'),
+    path("", views.messaging_main, name="messaging_main"),
+   # path('send/', views.send_message, name='send_message'),
+   # path('inbox/', views.inbox, name='inbox'),
+
     
 ]
--- messaging/views.py
+++ messaging/views.py
@@ -1,43 +1,72 @@
 from django.shortcuts import render, redirect #render → show HTML page/     redirect → move to another page
-from django.http import HttpResponse     
 from django.contrib.auth.models import User   #User → Django users
 from .models import Message   #Message → your model (database table)
-            
 
 
-def messaging_home(request):
-    return HttpResponse("Messaging homepage placeholder")
+def messaging_main(request):
+    view = request.GET.get('view', 'inbox')
 
-
-
-# To send a message
-def send_message(request):
-    
-    if request.method == "POST":
-        subject = request.POST['subject']
-        body = request.POST['body']
-        receiver_username = request.POST['receiver']
-
+    # Handle sending message
+    if request.method == 'POST':
+        subject = request.POST.get('subject')
+        body = request.POST.get('body')
+        receiver_username = request.POST.get('receiver')
         receiver = User.objects.get(username=receiver_username)
 
-        msg = Message(
-
+        Message.objects.create(
             subject=subject,
             body=body,
             sender=request.user,
             receiver=receiver
         )
+        return redirect('/messaging/?view=inbox')
 
-        msg.save()
+    # Prepare context
+    context = {
+        'view_type': view,
+        'users': User.objects.all()
+    }
+
+    if view == 'inbox':
+        context['messages'] = Message.objects.filter(receiver=request.user).order_by('-timeStamp')
+    elif view == 'sent':
+        context['sent_messages'] = Message.objects.filter(sender=request.user).order_by('-timeStamp')
+
+    return render(request, 'messaging/main.html', context)
 
 
-        return redirect('inbox')
-
-    users = User.objects.all()
-    return render(request, 'messaging/send_message.html', {'users': users})
 
 
 
-def inbox(request):
-    messages = Message.objects.filter(receiver=request.user).order_by('-timeStamp')
-    return render(request, 'messaging/inbox.html', {'messages': messages})
+
+# # To send a message
+# #def send_message(request):
+    
+#     if request.method == "POST":
+#         subject = request.POST['subject']
+#         body = request.POST['body']
+#         receiver_username = request.POST['receiver']
+
+#         receiver = User.objects.get(username=receiver_username)
+
+#         msg = Message(
+
+#             subject=subject,
+#             body=body,
+#             sender=request.user,
+#             receiver=receiver
+#         )
+
+#         msg.save()
+
+
+#         return redirect('inbox')
+
+#     users = User.objects.all()
+#     return render(request, 'messaging/send_message.html', {'users': users})
+
+
+
+# #def inbox(request):
+#     messages = Message.objects.filter(receiver=request.user).order_by('-timeStamp')
+#     return render(request, 'messaging/inbox.html', {'messages': messages})
