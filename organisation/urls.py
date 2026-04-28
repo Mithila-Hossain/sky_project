af7b69983574716fdafc497a776aed3d508afc47
Bereun-1122 <bernardvecino06@gmail.com>
2026-04-28 19:55:22 +0100

    updates

--- organisation/urls.py
+++ organisation/urls.py
@@ -1,6 +1,9 @@
+# Author: Bernard Vecino w19733959
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path("", views.organisation_home, name="organisation_home"),
+    path('departments/<int:dept_id>/', views.department_detail, name='department_detail'),
 ]
--- organisation/views.py
+++ organisation/views.py
@@ -1,7 +1,31 @@
+# Author: Bernard Vecino w19733959
+
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Department
+from teams.models import Dependency, Team
 
 # Create your views here.
 
 def organisation_home(request):
-    return HttpResponse("Organisation homepage placeholder")
+    departments = Department.objects.all()
+    dependencies = Dependency.objects.all() 
+    teams = Team.objects.all()
+
+    context = {
+        "departments": departments,
+        "dependencies": dependencies,
+        "teams": teams,
+        "page_title": "Organisation",
+        "calendar_title": "Team Connections"
+    }
+
+    return render(request, "organisation\organisation.html", context)
+
+def department_detail(request, dept_id):
+    department = Department.objects.get(id=dept_id)
+    teams = Team.objects.filter(department_id=dept_id)
+    return render(request, "organisation\department_detail.html", {
+        "department": department,
+        "teams": teams
+        })
