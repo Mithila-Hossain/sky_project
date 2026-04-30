28e50ba74fb9c416771ebfe545a652e98bc7116a
Sayid <usmanovsayid517@gmail.com>
2026-04-30 09:07:35 +0100

    Update urls.py

--- reports/urls.py
+++ reports/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # This makes the page available at /reports/
-    path('', views.report_home_view, name='report_page'),
+    path('', views.reports_view, name='reports'),
 ]
